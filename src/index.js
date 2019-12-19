d9aad889abc5b7205b4982c871ab495a87f16345
askhatxx <55058367+askhatxx@users.noreply.github.com>
2019-12-19 23:23:03 +0600

    Commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import AppReducer from './AppReducer';
+import AppClass from './AppClass';
 
-ReactDOM.render(<AppReducer />, document.getElementById('root'));
+ReactDOM.render(<AppClass/>, document.getElementById('root'));
