d81d23fbd82c671c76b9e6c2132ec8cd7c73f93f
askhatxx <askhatxx@gmail.com>
2019-12-20 12:13:51 +0600

    Commit

--- src/App.js
+++ src/App.js
@@ -1,120 +1,122 @@
-import React, {useState, useEffect} from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 import MainCars from './components/MainCars';
 
-export default function App() {
-    const getSizeArena = () => {
+export default class AppClass extends Component {
+    constructor(props) {
+        super(props);
+
+        this.botsCarsList = [
+            {color: '#0b86ec', width: 40, height: 60, left: 0, top: 0, id: 'bot1'},
+            {color: '#0b86ec', width: 40, height: 60, left: 0, top: 0, id: 'bot2'},
+            {color: '#0b86ec', width: 40, height: 60, left: 0, top: 0, id: 'bot3'},
+            {color: '#0b86ec', width: 40, height: 60, left: 0, top: 0, id: 'bot4'},
+            {color: '#0b86ec', width: 40, height: 60, left: 0, top: 0, id: 'bot5'},
+        ];
+        this.mainCarsList = [
+            {color: '#ec560b', width: 40, height: 60, left: 0, top: 0, id: 'player1'},
+            {color: '#d2d2d2', width: 40, height: 60, left: 0, top: 0, id: 'player2'},
+            {color: '#c2c2c2', width: 40, height: 60, left: 0, top: 0, id: 'player3'},
+        ];
+        const directionInit = [
+            {isLeft: false, isRight: false, isTop: false, isBottom: false, id: 'player1'},
+            {isLeft: false, isRight: false, isTop: false, isBottom: false, id: 'player2'},
+            {isLeft: false, isRight: false, isTop: false, isBottom: false, id: 'player3'},
+        ];
+
+        this.direction = directionInit.slice();
+
+        this.keys  = [
+            {left: 'KeyA', right: 'KeyD', top: 'KeyW', bottom: 'KeyS'},
+            {left: 'ArrowLeft', right: 'ArrowRight', top: 'ArrowUp', bottom: 'ArrowDown'},
+            {left: 'KeyH', right: 'KeyK', top: 'KeyU', bottom: 'KeyJ'},
+        ];
+
+        this.config = {
+            quantityBots: 3,
+            quantityPlayers: 2,
+            sizeArena: this.getSizeArena(),
+            gameOver: false,
+            speedBots: 3,
+            speedPlayers: 4,
+        };
+
+        this.state = {
+            bots: this.initBots(this.config.quantityBots),
+            players: this.initPlayers(this.config.quantityPlayers),
+        };
+    }
+
+    initBots = (quantity) => {
+        return this.botsCarsList.slice(0, quantity).map((item, index) => {
+            return this.randomPosition(item, index + 1);
+        });
+    }
+
+    initPlayers = (quantity) => {
+        return this.mainCarsList.slice(0, quantity).map((item, index) => {
+            const left = Math.round((this.config.sizeArena.width / (quantity + 1) * (index + 1)) - (item.width / 2));
+            const top = Math.round(this.config.sizeArena.height - item.height - 10);
+
+            return {...item, left, top};
+        });
+    }
+
+    getSizeArena = () => {
         const {clientWidth, clientHeight} = document.documentElement;
         const width = clientWidth > 1000 ? 900 : Math.round(clientWidth / 100 * 90);
         const height = Math.round(clientHeight / 100 * 90);
         
         return {width, height};
-    };
+    }
 
-    const initConfig = () => {
-        const sizeArena = getSizeArena();
-        const speedBots = 3;
-        const speedPlayers = 4;
-        const quantityBots = 1;
-        const quantityPlayers = 2;
-        const gameOver = false;
+    componentDidMount() {
+        document.addEventListener('keydown', this.keyDown);
+        document.addEventListener('keyup', this.keyUp);
+        this.renderGame();
+    }
 
-        return {sizeArena, speedBots, speedPlayers, quantityBots, quantityPlayers, gameOver};
-    };
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.keyDown);
+        document.removeEventListener('keyup', this.keyUp);
+    }
 
-    const botsCarsList = [
-        {color: '#0b86ec', width: 40, height: 60, left: 0, top: 0, id: 'bot1'},
-        {color: '#0b86ec', width: 40, height: 60, left: 0, top: 0, id: 'bot2'},
-        {color: '#0b86ec', width: 40, height: 60, left: 0, top: 0, id: 'bot3'},
-        {color: '#0b86ec', width: 40, height: 60, left: 0, top: 0, id: 'bot4'},
-        {color: '#0b86ec', width: 40, height: 60, left: 0, top: 0, id: 'bot5'},
-    ];
+    keyDown = (event) => {
+        for (let i = 0; i < this.config.quantityPlayers; i++) {
+            if (event.code === this.keys[i].left && !this.direction[i].isLeft) {
+                this.direction[i].isLeft = true;
+            } else if (event.code === this.keys[i].right && !this.direction[i].isRight) {
+                this.direction[i].isRight = true;
+            } else if (event.code === this.keys[i].top && !this.direction[i].isTop) {
+                this.direction[i].isTop = true;
+            } else if (event.code === this.keys[i].bottom && !this.direction[i].isBottom) {
+                this.direction[i].isBottom = true;
+            }
+        }
+    }
 
-    const randomPosition = (item, order) => {
-        const left = Math.round((config.sizeArena.width - item.width) * Math.random());
+    keyUp = (event) => {
+        for (let i = 0; i < this.config.quantityPlayers; i++) {
+            if (event.code === this.keys[i].left) {
+                this.direction[i].isLeft = false;
+            } else if (event.code === this.keys[i].right) {
+                this.direction[i].isRight = false;
+            } else if (event.code === this.keys[i].top) {
+                this.direction[i].isTop = false;
+            } else if (event.code === this.keys[i].bottom) {
+                this.direction[i].isBottom = false;
+            }
+        }
+    }
+
+    randomPosition = (item, order) => {
+        const left = Math.round((this.config.sizeArena.width - item.width) * Math.random());
         const top = -120 * order;
 
         return {...item, left, top};
-    };
+    }
 
-    const initBots = (quantity) => {
-        return botsCarsList.slice(0, quantity).map((item, index) => {
-            return randomPosition(item, index + 1);
-        });
-    };
-
-    const mainCarsList = [
-        {color: '#ec560b', width: 40, height: 60, left: 0, top: 0, id: 'player1'},
-        {color: '#d2d2d2', width: 40, height: 60, left: 0, top: 0, id: 'player2'},
-        {color: '#c2c2c2', width: 40, height: 60, left: 0, top: 0, id: 'player3'},
-    ];
-
-    const initPlayers = (quantity) => {
-        return mainCarsList.slice(0, quantity).map((item, index) => {
-            const left = Math.round((config.sizeArena.width / (quantity + 1) * (index + 1)) - (item.width / 2));
-            const top = Math.round(config.sizeArena.height - item.height - 10);
-
-            return {...item, left, top};
-        });
-    };
-
-    const directionInit = [
-        {isLeft: false, isRight: false, isTop: false, isBottom: false},
-        {isLeft: false, isRight: false, isTop: false, isBottom: false},
-        {isLeft: false, isRight: false, isTop: false, isBottom: false},
-    ];
-    
-    const config = initConfig();
-    const [bots, setBots] = useState(initBots(config.quantityBots));
-    const [players, setPlayers] = useState(initPlayers(config.quantityPlayers));
-    const direction = directionInit.slice();
-
-    const keys = [
-        {left: 'KeyA', right: 'KeyD', top: 'KeyW', bottom: 'KeyS'},
-        {left: 'ArrowLeft', right: 'ArrowRight', top: 'ArrowUp', bottom: 'ArrowDown'},
-        {left: 'KeyH', right: 'KeyK', top: 'KeyU', bottom: 'KeyJ'},
-    ];
-
-    const keyDown = (event) => {
-        for (let i = 0; i < config.quantityPlayers; i++) {
-            if (event.code === keys[i].left && !direction[i].isLeft) {
-                direction[i].isLeft = true;
-            } else if (event.code === keys[i].right && !direction[i].isRight) {
-                direction[i].isRight = true;
-            } else if (event.code === keys[i].top && !direction[i].isTop) {
-                direction[i].isTop = true;
-            } else if (event.code === keys[i].bottom && !direction[i].isBottom) {
-                direction[i].isBottom = true;
-            }
-        }
-    };
-
-    const keyUp = (event) => {
-        for (let i = 0; i < config.quantityPlayers; i++) {
-            if (event.code === keys[i].left) {
-                direction[i].isLeft = false;
-            } else if (event.code === keys[i].right) {
-                direction[i].isRight = false;
-            } else if (event.code === keys[i].top) {
-                direction[i].isTop = false;
-            } else if (event.code === keys[i].bottom) {
-                direction[i].isBottom = false;
-            }
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener('keydown', keyDown);
-        document.addEventListener('keyup', keyUp);
-        renderGame();
-        
-        return () => {
-            document.removeEventListener('keydown', keyDown);
-            document.removeEventListener('keyup', keyUp);
-        };
-    }, []);
-
-    const collisionCheck = (car1, car2) => {
+    collisionCheck = (car1, car2) => {
 		if (car1.top - (car2.top + car2.height) < 0 && (car1.top + car1.height) - car2.top > 0) {
             if (car1.left - (car2.left + car2.width) < 0 && (car1.left + car1.width) - car2.left > 0) {
                 return true;
@@ -122,80 +124,89 @@ export default function App() {
 		}
         
 		return false;
-    };
-    
-    const collision = () => {
-        setBots(prevBots => {
-        setPlayers(prevPlayers => {
+    }
+
+    collision = ({bots, players}) => {        
             let lostIndex = new Set();
 
-            prevPlayers.forEach((item, index) => {
-                prevPlayers.forEach((itemCheck, indexCheck) => {
+            players.forEach((item, index) => {
+                players.forEach((itemCheck, indexCheck) => {
                     if (index !== indexCheck) {
-                        if (collisionCheck(item, itemCheck)) {lostIndex.add(index);}
+                        if (this.collisionCheck(item, itemCheck)) {lostIndex.add(index);}
                     }
                 });
                 
-                prevBots.forEach((itemBot, index) => {console.log(lostIndex)
-                    if (collisionCheck(item, itemBot)) {lostIndex.add(index);}
+                bots.forEach((itemBot) => {
+                    if (this.collisionCheck(item, itemBot)) {lostIndex.add(index);}
                 });
             });
-            
-            return prevPlayers.filter((item, index) => {
+
+            players = players.filter((item, index) => {
                 if (lostIndex.has(index)) {
                     return false;
                 }
                 return true;
             });
-        });
-        return [...prevBots];
-        });
-    };
 
-    const renderGame = () => {
-        collision();
-        
-        setBots(prevBots => prevBots.map(item => {
-            const top = item.top + config.speedBots;
+            return {bots, players};
+    }
 
-            if (top > config.sizeArena.height) {
-                return randomPosition(item, 1);
-            }
+    renderGame = () => {
+        this.setState(prevState => {
+            const bots = prevState.bots.map(item => {
+                const top = item.top + this.config.speedBots;
+    
+                if (top > this.config.sizeArena.height) {
+                    return this.randomPosition(item, 1);
+                }
+                
+                return {...item, top};
+            });
             
-            return {...item, top};
-        }));
+            const players = prevState.players.map((item) => {
+                let {left, top, width, height} = item;
+                const index = this.direction.findIndex(dir => dir.id === item.id);
+    
+                if (this.direction[index].isLeft) left -= this.config.speedPlayers;
+                if (this.direction[index].isRight) left += this.config.speedPlayers;
+                if (this.direction[index].isTop) top -= this.config.speedPlayers;
+                if (this.direction[index].isBottom) top += this.config.speedPlayers;
+    
+                if (left < 0) left = 0;
+                if (left > this.config.sizeArena.width - width) left = this.config.sizeArena.width - width;
+                if (top < 0) top = 0;
+                if (top > this.config.sizeArena.height - height) top = this.config.sizeArena.height - height;
+    
+                return {...item, left, top};
+            });
+
+            const result = this.collision({bots, players});
+
+            if (result.players.length === 0) {
+                this.config.gameOver = true;
+            }
+            console.log(this.config.gameOver);
+
+            return result;
+        });
         
-        setPlayers(prevPlayers => prevPlayers.map((item, index) => {
-            let {left, top, width, height} = item;
-
-            if (direction[index].isLeft) left -= config.speedPlayers;
-            if (direction[index].isRight) left += config.speedPlayers;
-            if (direction[index].isTop) top -= config.speedPlayers;
-            if (direction[index].isBottom) top += config.speedPlayers;
-
-            if (left < 0) left = 0;
-            if (left > config.sizeArena.width - width) left = config.sizeArena.width - width;
-            if (top < 0) top = 0;
-            if (top > config.sizeArena.height - height) top = config.sizeArena.height - height;
-
-            return {...item, left, top};
-        }));
-        
-        if (!config.gameOver) {
-            config.speedBots += 0.005;
-            //requestAnimationFrame(renderGame);
-            setTimeout(renderGame, 1000 / 30);
+        if (!this.config.gameOver) {
+            this.config.speedBots += 0.005;
+            requestAnimationFrame(this.renderGame);
+            //setTimeout(this.renderGame, 1000 / 50);
         }
-    };
+    }
 
-    return (
-        <GameWrapper>
-            <GameArena size={config.sizeArena}>
-                <MainCars cars={bots}/>
-                <MainCars cars={players}/>
-            </GameArena>
-        </GameWrapper>
-    );
+    render() {
+        return (
+            <GameWrapper>
+                <GameArena size={this.config.sizeArena}>
+                    <MainCars cars={this.state.bots}/>
+                    <MainCars cars={this.state.players}/>
+                </GameArena>
+            </GameWrapper>
+        );
+    }
 }
 
 const GameWrapper = styled.div`
--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import AppReducer from './AppReducer';
-import AppClass from './AppClass';
 
-ReactDOM.render(<AppClass/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
